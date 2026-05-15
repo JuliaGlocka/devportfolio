dcfd8cd625a3ead8c8e5b820ce0d68470fb71ccc
glock <glockajulia@gmail.com>
2026-05-15 19:59:28 +0200

    change download file name

--- js/script.js
+++ js/script.js
@@ -387,9 +387,9 @@ matrixButton.addEventListener("click", () => {
 
 // === CV BUTTON ===
 
-/* document.getElementById("cvButton").addEventListener("click", () => {
-    alert("You have downloaded the CV");
-}); */
+document.getElementById("cvButton").addEventListener("click", () => {
+    alert("You have downloaded Julia's CV!");
+});
 
 
 // === MATRIX BACKGROUND EFFECT ===
